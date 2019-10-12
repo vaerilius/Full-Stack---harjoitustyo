92f1c06bfadbf8959144ebd58f16e6eff3af0c77
Vaerilius <tjohannest@gmail.com>
2019-10-12 10:14:40 +0300

    Backend: user jobProvided & get job by id

--- README.md
+++ README.md
@@ -17,4 +17,6 @@
 | 9.10  | 1 | dir structure & landing page & navbar & job reducer and service & jobs / jobList component|
 | 11.10 | 2 | Backend fix, new model & controller Job / user login & auth / modified use model |
 | 11.10 | 0.5h | Backend modify && fix |
+| 12.10 | 1 | Backend: user jobProvided & get job by id |
+
 
--- backend/controllers/jobs.js
+++ backend/controllers/jobs.js
@@ -6,45 +6,59 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-      return authorization.substring(7)
-    }
-    return null
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
   }
-  
+  return null
+}
+
 
 jobsRouter.get('/', async (request, response) => {
   const jobs = await Job.find({})
-  .populate('user', { picture: 1, name: 1, status: 1})
+    .populate('user', { picture: 1, name: 1, status: 1, jobsProvided: 1 })
   response.json(jobs.map(job => job.toJSON()))
 
 })
+jobsRouter.get('/:id', async (req, res, next) => {
+  try {
+    const job = await Job.findById(req.params.id)
+    if (job) {
+      res.json(job.toJSON())
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
 
 jobsRouter.post('/', async (request, response, next) => {
-
   const body = request.body
   const token = getTokenFrom(request)
 
   try {
     const decodedToken = jwt.verify(token, config.SECRET)
-
     if (!token || !decodedToken.id) {
       return response.status(401).json({ error: 'token missing or invalid' })
-
     }
     const user = await User.findById(decodedToken.id)
+
+    if (!user.jobProvider) {
+      response.status(401).json({ error: 'Only job provider can add job advertisement' })
+    }
+
     const job = new Job({
-        title: body.title,
-        description: body.description,
-        canditates: [],
-        company: body.company,
-        jobProvider: user.id
+      title: body.title,
+      description: body.description,
+      canditates: [],
+      company: body.company,
+      jobProvider: user.id
     })
-    console.log(job);
 
     const savedJob = await job.save()
-    user.jobsProvided.concat(savedJob._id)
+    user.jobsProvided = user.jobsProvided.concat(savedJob)
+    await user.save()
     response.json(savedJob.toJSON())
   } catch (error) {
     next(error)
