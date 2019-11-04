18300dafbb5440af390cf3b80bdd82d3fe0342e1
vaerilius <tjohannest@gmail.com>
2019-11-04 09:08:08 +0200

    login test

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -4,35 +4,42 @@ const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
-loginRouter.post('/', async (request, response) => {
+loginRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findOne({ username: body.username })
-  const passwordCorrect = user === null
-    ? false
-    : await bcrypt.compare(body.password, user.passwordHash)
+  try {
+    const user = await User.findOne({ username: body.username })
 
-  if (!(user && passwordCorrect)) {
-    return response.status(401).json({
-      error: 'invalid username or password'
-    })
-  }
-
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  }
-
-  const token = jwt.sign(userForToken, process.env.SECRET)
-  response
-    .status(200)
-    .send({
-      token,
+    const passwordCorrect = user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash)
+  
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: 'invalid username or password'
+      })
+    }
+  
+    const userForToken = {
       username: user.username,
-      name: user.name,
-      picture: user.picture,
-      id: user._id
-    })
+      id: user._id,
+    }
+  
+    const token = jwt.sign(userForToken, process.env.SECRET)
+    response
+      .status(200)
+      .send({
+        token,
+        username: user.username,
+        name: user.name,
+        picture: user.picture,
+        id: user._id
+      })
+  } catch (error) {
+    next(error)
+  }
+
+
 })
 
 module.exports = loginRouter
--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -14,6 +14,7 @@ const unknownEndpoint = (request, response) => {
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
+  logger.error(error.name)
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
@@ -23,6 +24,12 @@ const errorHandler = (error, request, response, next) => {
     return response.status(401).json({
       error: 'invalid token'
     })
+  }
+  else if (error.name === 'Error') {
+    return response.status(400).json({ error: error.message })
+  }
+  else if (error.name === 'TypeError') {
+    return response.status(400).json({ error: 'invalid input data' })
   }
 
   next(error)
