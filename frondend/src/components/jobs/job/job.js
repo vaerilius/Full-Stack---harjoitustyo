1f1bbac77bd356ee794dba785a044cb3f38b9a54
vaerilius <tjohannest@gmail.com>
2019-11-22 09:04:22 +0200

    22.11 job manage

--- frondend/src/components/jobs/job/job.js
+++ frondend/src/components/jobs/job/job.js
@@ -2,19 +2,31 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { addCandidate, removeJobAdversement } from '../../../reducers/jobReducer'
 import { Link } from 'react-router-dom'
+import { useField } from '../../../hooks/formHook'
 
 const Job = (props) => {
 
   const [lastchange, setlastChange] = useState(null)
   const [createdAt, setCreatedAt] = useState(null)
+  const [manage, setManage] = useState(false)
+  const [gridClass, setGridClass] = useState('col-sm-12')
+  const [title, resetTitle] = useField('text')
+  const [description, resetDescription] = useField('text')
 
   useEffect(() => {
-    console.log('updateAT')
     setlastChange(props.job.updatedAt.split('T')[0])
   }, [props.job.candidates])
   useEffect(() => {
     setCreatedAt(props.job.createdAt.split('T')[0])
   }, [])
+  useEffect(() => {
+    if (!manage) {
+      setGridClass('col-md-12')
+    } else {
+      setGridClass('col-sm-12 col-md-6')
+
+    }
+  }, [manage])
 
 
   if (!props.job) {
@@ -38,86 +50,122 @@ const Job = (props) => {
 
   return (
 
-    <div className="card m-5 card-hover">
-      <div className="card-header">
-        Job advertisement
-      </div>
-
-      <img
-        className="img-fluid"
-        alt="..."
-        style={{ maxHeight: '15rem', maxWidth: '500px' }}
-        src={props.job.picture} />
-      <div className="card-body">
-        <h5 className="card-title">{props.job.title}</h5>
-        <p className="card-text">{props.job.description}</p>
-      </div>
-      <div className="list-group list-group-flush">
-
-        {props.job.candidates
-          .map(c =>
-            <div key={c.id} className="list-group-item list-group-item-action">
-              <Link to={`/candidates/${c.id}`}>
-                <div className="d-flex w-100 justify-content-between">
-                  <h5 className="m-3">{c.name}</h5>
-                  <img src={c.picture}
-                    className="m-3 " style={{ height: '2rem' }} alt={c.name} />
-                </div>
-              </Link>
+    <div className="container">
+      <div className="row">
+        <div className={gridClass}>
+          <div className="card m-5 card-hover">
+            <div className="card-header">
+              Job advertisement:
+              <h5 className="card-title">{props.job.title}</h5>
             </div>
-          )}
-      </div>
-      <div className="card-body">
-        {!props.user.jobProvider
+            <img
+              className="rounded mx-auto d-block my-5"
+              alt="..."
+              style={{ maxHeight: '18rem', maxWidth: '500px' }}
+              src={props.job.picture} />
+            <div className="card-body">
+              <p className="card-text">{props.job.description}</p>
+            </div>
+            <div className="list-group list-group-flush">
+
+              {props.job.candidates
+                .map(c =>
+                  <div key={c.id} className="list-group-item list-group-item-action">
+                    <Link to={`/candidates/${c.id}`}>
+                      <div className="d-flex w-100 justify-content-between">
+                        <h5 className="m-3">{c.name}</h5>
+                        <img src={c.picture}
+                          className="m-3 " style={{ height: '2rem' }} alt={c.name} />
+                      </div>
+                    </Link>
+                  </div>
+                )}
+            </div>
+            <div className="card-body">
+              {!props.user.jobProvider
+                ?
+                <button
+                  type="button"
+                  disabled={isDisabled}
+                  className='btn btn-success  btn-md btn-block'
+                  onClick={onAddCandidate}>Add me!</button>
+                : null
+
+              }
+              {props.user.jobProvider
+                ?
+                <button
+                  type="button"
+                  className='btn btn-danger btn-md btn-block'
+                  onClick={() => setManage(!manage)}
+                >Manage</button>
+                : null
+
+              }
+
+              {props.user.jobProvider
+                ?
+                <Link to={'/jobs/'}>
+                  <button type="button"
+                    className="btn btn-danger btn-md btn-block mt-2"
+                    onClick={onRemoveJobAdversement}
+                  >Remove Job</button>
+                </Link>
+
+                :
+                null
+              }
+
+            </div>
+            <div className="card-footer text-muted">
+
+              Created at: {createdAt}
+            </div>
+            <div className="card-footer text-muted">
+              last update: {lastchange}
+            </div>
+
+            <Link to={'/jobs/'}>
+              <button type="button"
+                className="btn btn-primary btn-md btn-block">
+                Back to jobs</button>
+            </Link>
+
+
+          </div>
+        </div>
+
+        {manage
           ?
-          <button
-            type="button"
-            disabled={isDisabled}
-            className='btn btn-success  btn-md btn-block'
-            onClick={onAddCandidate}>Add me!</button>
+          <div className="col-sm-12 col-md-6">
+            <div className="card m-5 card-hover">
+              <div className="card-header">
+                Manage job advertisement
+              </div>
+              <div className="card-body">
+                <form>
+                  <div className="form-group">
+                    <label htmlFor="exampleInputEmail1">New Title</label>
+                    <input {...title} className="form-control" id="title" placeholder="Enter new title" />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="desc">New description</label>
+                    <input {...description} className="form-control" id="desc" placeholder="Description" />
+                  </div>
+
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+              </div>
+            </div>
+          </div>
           : null
-
-        }
-        {props.user.jobProvider
-          ?
-          <a
-            type="button"
-            className='btn btn-danger btn-md btn-block'
-          >Manage</a>
-          : null
-
         }
 
-        {props.user.jobProvider
-          ?
-          <Link to={'/jobs/'}>
-            <button type="button"
-              className="btn btn-danger btn-md btn-block mt-2"
-              onClick={onRemoveJobAdversement}
-            >Remove Job</button>
-          </Link>
-
-          :
-          null
-        }
 
       </div>
-      <div className="card-footer text-muted">
-
-        Created at: {createdAt}
-      </div>
-      <div className="card-footer text-muted">
-        last update: {lastchange}
-      </div>
-
-      <Link to={'/jobs/'}>
-        <button type="button"
-          className="btn btn-primary btn-md btn-block">
-          Back to jobs</button>
-      </Link>
-
-
     </div>
+
+
   )
 }
 const mapStateToProps = (state, ownProps) => {
